a3ff5e2b3917cb63390332aca1f2e08d57897eef
Kai Cao <caokai_cgs@163.com>
2025-04-06 00:54:38 +0300

    - Updated version number format and global logger setting.

--- __init__.py
+++ __init__.py
@@ -1,3 +1,20 @@
 import ASD_File_Reader
 
-__version__ = '1.0.0'
+main_version = 1
+major_version = 0
+minor_version = 1
+__version__ = f"{main_version}.{major_version}.{minor_version}"
+
+
+# Set up Global logger for the package
+import os
+import logging
+from .src.loggerSetup import setup_logging
+logFile=os.path.join(os.path.dirname(__file__), 'ASD_File_Reader.log')
+# setup_logging(logFile, log_level=logging.CRITICAL) 
+# setup_logging(logFile, log_level=logging.DEBUG)
+setup_logging(logFile, log_level=logging.INFO)
+globalLogger = logging.getLogger('Global Logger')
+
+
+__all__ = ["ASDFile"]
