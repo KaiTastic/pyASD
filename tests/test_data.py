e2eea26d12634866996689fa947e61632c3ca1a3
Kai Cao <caokai_cgs@163.com>
2025-04-06 13:57:12 +0300

    - Modified test_data module.

--- tests/test_data.py
+++ tests/test_data.py
@@ -4,8 +4,6 @@ List of sample data files for testing purposes.
 import os
 
 # Sample data files
-
-
 # All Sample data files for testing purposes
 
 def find_files_with_extension(directory):
@@ -17,9 +15,3 @@ def find_files_with_extension(directory):
     return matching_files
 
 all_asd_data_files = find_files_with_extension('.')
-# # Remove the leading './' from the file paths
-# all_asd_data_files = [path[2:] if path.startswith('./') else path for path in all_asd_data_files]
-# # Remove the leading '.\\' from the file paths
-# all_asd_data_files = [path[2:] if path.startswith('.\\') else path for path in all_asd_data_files]
-
-# print(all_asd_data_files)
